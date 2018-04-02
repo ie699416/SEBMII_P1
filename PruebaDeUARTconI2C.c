02aa8e55dcd93dde24deaae464bd3c3beb0c4a9b
RobinSalgado <ie686481@iteso.mx>
2018-04-02 00:53:21 -0500

    Escribe strings Y manda a UART
    La primera vez que imprime, imprime basura.

---
+++ PruebaDeUARTconI2C.c
@@ -0,0 +1,266 @@
+/*
+ * Copyright (c) 2017, NXP Semiconductor, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of NXP Semiconductor, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file    MK64FN1M0xxx12_EEPROM.c
+ * @brief   Application entry point.
+ */
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MK64F12.h"
+#include "fsl_debug_console.h"
+#include "FreeRTOS.h"
+#include "fsl_port.h"
+#include "fsl_i2c.h"
+#include "Task.h"
+#include "fsl_uart.h"
+#include "queue.h"
+
+/* TODO: insert other include files here. */
+
+/* TODO: insert other definitions and declarations here. */
+
+#define I2C0_BASEADDR I2C0
+#define RTC_slave_address 0x51
+#define I2C_MASTER_CLK I2C0_CLK_SRC
+#define BUFFER_SIZE 2
+#define EEPROM_SLAVE_ADDR 0x50
+
+QueueHandle_t g_EEPROM_read;
+volatile bool txFinished;
+volatile bool rxFinished;
+
+void UART1_UserCallback(UART_Type *base, uart_handle_t *handle, status_t status,
+		void *userData) {
+	userData = userData;
+	if (kStatus_UART_TxIdle == status) {
+		txFinished = true;
+	}
+	if (kStatus_UART_RxIdle == status) {
+		rxFinished = true;
+
+	}
+}
+void UART_putString(uint8_t * dataToSend ) {
+
+
+	uart_handle_t g_uartHandle;
+	uart_transfer_t sendXfer;
+	uint8_t index = 0;
+
+	for (index = 0; (dataToSend[index] != '\0'); index++) {
+
+	}
+
+	uint8_t sendData[index];
+
+	UART_TransferCreateHandle(UART1, &g_uartHandle, UART1_UserCallback,
+	NULL);
+
+	for (index = 0; (dataToSend[index] != '\0'); index++) {
+		sendData[index] = dataToSend[index];
+	}
+
+	// Prepare to send.
+	sendXfer.data = sendData;
+	sendXfer.dataSize = sizeof(sendData) / sizeof(sendData[0]);
+	txFinished = false;
+	// Send out.
+
+	UART_TransferSendNonBlocking(UART1, &g_uartHandle, &sendXfer);
+	// Wait send finished.
+	while (!txFinished) {
+	}
+
+}
+void UART1_init_task(void * arg) {
+
+	CLOCK_EnableClock(kCLOCK_PortC);
+
+	port_pin_config_t config_uart = { kPORT_PullDisable, kPORT_SlowSlewRate,
+			kPORT_PassiveFilterDisable, kPORT_OpenDrainDisable,
+			kPORT_LowDriveStrength, kPORT_MuxAlt3, kPORT_UnlockRegister, };
+
+	PORT_SetPinConfig(PORTC, 4, &config_uart);
+	PORT_SetPinConfig(PORTC, 3, &config_uart);
+
+	CLOCK_EnableClock(kCLOCK_Uart1);
+	uart_config_t user_config;
+	UART_GetDefaultConfig(&user_config);
+	user_config.baudRate_Bps = 9600;
+	user_config.enableTx = true;
+	user_config.enableRx = true;
+
+	UART_Init(UART1, &user_config, CLOCK_GetFreq(UART1_CLK_SRC));
+	vTaskDelete(NULL);
+}
+
+//TaskHandle_t Handle_I2CInit;
+i2c_master_transfer_t masterXfer;
+i2c_master_handle_t g_m_handle;
+volatile bool g_MasterCompletionFlag = false;
+static void i2c_master_callback(I2C_Type *base, i2c_master_handle_t *handle,
+		status_t status, void *userData) {
+	//Signal transfer success when received success status.
+	if (status == kStatus_Success) {
+		g_MasterCompletionFlag = true;
+	}
+}
+
+void Read_EEPROM(void * arg) {
+
+	CLOCK_EnableClock(kCLOCK_I2c0);
+	CLOCK_EnableClock(kCLOCK_PortE);
+
+	port_pin_config_t config_i2c = { kPORT_PullDisable, kPORT_SlowSlewRate,
+			kPORT_PassiveFilterDisable, kPORT_OpenDrainDisable,
+			kPORT_LowDriveStrength, kPORT_MuxAlt5, kPORT_UnlockRegister, };
+
+	PORT_SetPinConfig(PORTE, 24, &config_i2c);
+	PORT_SetPinConfig(PORTE, 25, &config_i2c);
+
+	i2c_master_config_t masterConfig;
+	I2C_MasterGetDefaultConfig(&masterConfig);
+	I2C_MasterInit(I2C0_BASEADDR, &masterConfig, CLOCK_GetFreq(kCLOCK_BusClk));
+
+			i2c_master_handle_t g_m_handle;
+			I2C_MasterTransferCreateHandle(I2C0_BASEADDR, &g_m_handle,
+					i2c_master_callback, NULL);
+
+			i2c_master_transfer_t masterXfer;
+
+	//	    uint8_t read_buffer;
+		    uint8_t read_buffer[7];
+			masterXfer.slaveAddress = EEPROM_SLAVE_ADDR;
+			masterXfer.direction = kI2C_Read;
+			masterXfer.subaddress = 0x0000;
+			masterXfer.subaddressSize = 2;
+			masterXfer.data = read_buffer;
+			masterXfer.dataSize = 7;
+			masterXfer.flags = kI2C_TransferDefaultFlag;
+
+			I2C_MasterTransferNonBlocking(I2C0, &g_m_handle, &masterXfer);
+			vTaskDelay(pdMS_TO_TICKS(500));
+
+
+//	uint8_t sendData[];
+
+	for (;;) {
+
+		I2C_MasterTransferNonBlocking(I2C0, &g_m_handle, &masterXfer);
+		while (!g_MasterCompletionFlag) {
+				}
+		g_MasterCompletionFlag = false;
+
+//		sendData[0] = ((read_buffer & 0xF0) >> 4) + '0';
+//		sendData[1] = (read_buffer & 0x0F) + '0';
+//		sendData[2] = '\0';
+
+		//UART_putString("\r");
+		UART_putString(read_buffer);
+		vTaskDelay(pdMS_TO_TICKS(1000));
+
+	}
+
+}
+
+void Write_EEPROM(void * arg) {
+	CLOCK_EnableClock(kCLOCK_I2c0);
+	CLOCK_EnableClock(kCLOCK_PortE);
+
+	port_pin_config_t config_i2c = { kPORT_PullDisable, kPORT_SlowSlewRate,
+			kPORT_PassiveFilterDisable, kPORT_OpenDrainDisable,
+			kPORT_LowDriveStrength, kPORT_MuxAlt5, kPORT_UnlockRegister, };
+
+	PORT_SetPinConfig(PORTE, 24, &config_i2c);
+	PORT_SetPinConfig(PORTE, 25, &config_i2c);
+
+	i2c_master_config_t masterConfig;
+	I2C_MasterGetDefaultConfig(&masterConfig);
+	I2C_MasterInit(I2C0_BASEADDR, &masterConfig, CLOCK_GetFreq(kCLOCK_BusClk));
+
+
+	I2C_MasterTransferCreateHandle(I2C0_BASEADDR, &g_m_handle,
+			i2c_master_callback, NULL);
+
+
+
+	uint8_t data_buff[] ={"culito"} ;
+
+	//Init I2C master.
+	masterXfer.slaveAddress = EEPROM_SLAVE_ADDR;
+	masterXfer.direction = kI2C_Write;
+	masterXfer.subaddress = 0x0000;
+	masterXfer.subaddressSize = 2;
+	masterXfer.data = data_buff;
+	masterXfer.dataSize = 7;
+	masterXfer.flags = kI2C_TransferDefaultFlag;
+
+	I2C_MasterTransferNonBlocking(I2C0_BASEADDR, &g_m_handle, &masterXfer);
+
+	//Wait for transfer completed.
+	while (!g_MasterCompletionFlag) {
+	}
+
+	for (;;) {
+
+		vTaskDelay(pdMS_TO_TICKS(100));
+
+	}
+}
+
+int main(void) {
+
+	/* Init board hardware. */
+	BOARD_InitBootPins();
+	BOARD_InitBootClocks();
+	BOARD_InitBootPeripherals();
+	/* Init FSL debug console. */
+	BOARD_InitDebugConsole();
+
+//    xTaskCreate(I2C_init, "I2C_init", 200, NULL, configMAX_PRIORITIES - 1, NULL);
+
+	xTaskCreate(UART1_init_task, "UART1_init", 200, NULL, configMAX_PRIORITIES,
+	NULL);
+//	xTaskCreate(Write_EEPROM, "Write_EEPROM", 200, NULL,configMAX_PRIORITIES - 1,
+//	NULL);
+	xTaskCreate(Read_EEPROM, "Read_EEPROM", 200, NULL, configMAX_PRIORITIES - 2,
+	NULL);
+
+	vTaskStartScheduler();
+
+	while (1) {
+
+	}
+	return 0;
+}
