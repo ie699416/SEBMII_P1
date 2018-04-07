4fd315af7b6e213166f9349cd715dd7860f343e5
ie699416 <ie699416@iteso.mx>
2018-04-06 20:05:09 -0500

    Breaks sin solucionar

---
+++ main.c
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2017, NXP Semiconductor, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of NXP Semiconductor, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file    SEBMII_P1.c
+ * @brief   Application entry point.
+ */
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MK64F12.h"
+#include "fsl_debug_console.h"
+#include "main_tasks.h"
+#include "FreeRTOS.h"
+#include "fsl_port.h"
+
+/*
+ * @brief   Application entry point.
+ */
+
+int main(void) {
+
+	/* Init board hardware. */
+	BOARD_InitBootPins();
+	BOARD_InitBootClocks();
+	BOARD_InitBootPeripherals();
+	/* Init FSL debug console. */
+	BOARD_InitDebugConsole();
+
+	xTaskCreate(sInitLCD_task, "LCD Nokia init", 200, NULL,
+	configMAX_PRIORITIES, NULL);
+
+	CLOCK_EnableClock(kCLOCK_PortC);
+	CLOCK_EnableClock(kCLOCK_Uart1);
+
+	port_pin_config_t config_uart = { kPORT_PullDisable, kPORT_SlowSlewRate,
+			kPORT_PassiveFilterDisable, kPORT_OpenDrainDisable,
+			kPORT_LowDriveStrength, kPORT_MuxAlt3, kPORT_UnlockRegister, };
+
+	PORT_SetPinConfig(PORTC, 4, &config_uart);
+	PORT_SetPinConfig(PORTC, 3, &config_uart);
+
+	NVIC_EnableIRQ(I2C0_IRQn);
+	NVIC_EnableIRQ(SPI0_IRQn);
+	NVIC_EnableIRQ(UART0_RX_TX_IRQn);
+	NVIC_EnableIRQ(UART1_RX_TX_IRQn);
+	NVIC_SetPriority(I2C0_IRQn, 5);
+	NVIC_SetPriority(SPI0_IRQn, 5);
+	NVIC_SetPriority(UART0_RX_TX_IRQn, 5);
+	NVIC_SetPriority(UART1_RX_TX_IRQn, 5);
+
+	vTaskStartScheduler();
+
+	while (1) {
+	}
+	return 0;
+}
